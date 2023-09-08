df3e51f320cce24f1184f593d5cd30609a1246bc
FirdausYNWA <firdausynwa5@gmail.com>
2023-09-08 16:49:15 +0700

    npx hardhad compiler

--- README.md
+++ README.md
@@ -1,15 +1,18 @@
 # STEP 10:46
+
 install nodejs 16
-install hardhad /npm install --save-dev hardhat/
+install hardhad == npm install --save-dev hardhat/
 term npx hardhad
 enter2
 
- trem npm install @openzeppelin/contracts --save
+trem npm install @openzeppelin/contracts --save
 
 # DEPLOYNFT
+
 FUNGSI FOLDER
 -contracts untuk membuat smart contrak
 -scripts untuk interaksi dengan smart contrak
 
 # web ACCESS
+
 https://hardhat.org/hardhat-runner/docs/getting-started#installation
--- contracts/firdaus.sol
+++ contracts/firdaus.sol
@@ -1,22 +1,33 @@
 //contract/firdaus.sol
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
-    
-import "@import "@openzeppelin/contracts/token/ERC721/extentions/ERC721Enumerable.sol";
-    import "@import "@openzeppelin/contracts/token/access/ownable.sol";
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 // ownable berfungsi untuk kita mengatur fungsi fungsi kita oleh owner
 
-contract firdaus is ERC721Enumerable,ownable{
-uin256 public constant MAX_SUPPLY =20;
+contract firdaus is ERC721Enumerable,Ownable{
+uint256 public constant MAX_SUPPLY =20;
+string private _baseTokenURI;
 
-constructor{} ERC721{"firdaus","SAL"}{
+constructor() ERC721("firdaus","SAL"){
 
 }
 
-fungtion mint{uin256 quantity_} external payable {
-    require{totalSupply{} + quantity_ <= MAX_SUPPLY, 'Max supply sudah terpenuhi'};
+function _baseURI() internal view virtual override returns (string memory){
+    return _baseTokenURI;
+}
+
+function setBaseURL(string calldata baseURI_) external onlyOwner{
+    _baseTokenURI = baseURI_;
+}
+ 
+function mint(uint256 quantity_) external payable {
+    require(totalSupply() + quantity_ <= MAX_SUPPLY, 'Max supply sudah terpenuhi');
     
-    for
+    for(uint256 i = 1;i <= quantity_;i ++){
+        _safeMint (msg.sender, totalSupply() + 1);
+    }
 
 }
 }
